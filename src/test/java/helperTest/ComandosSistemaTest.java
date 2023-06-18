7be11ecec873cba1f2486ebe2f35dbc7d2c5e322
GabrielGamaa <136938812+GabrielGamaa@users.noreply.github.com>
2023-06-17 21:13:11 -0300

    Add files via upload

---
+++ src/test/java/helperTest/ComandosSistemaTest.java
@@ -0,0 +1,131 @@
+package helperTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import helper.ComandosSistema;
+import insercoes.Aplicacao;
+import insercoes.Despesa;
+import insercoes.Emprestimo;
+import insercoes.Receita;
+import relatorio.*;
+class ComandosSistemaTest {
+
+    private ComandosSistema comandosSistema;
+   private  FabricaRelatorio fr;
+
+    @BeforeEach
+    void setUp() {
+        comandosSistema = new ComandosSistema();
+        fr = new FabricaRelatorio();
+    }
+
+    @AfterEach
+    void tearDown() {
+        
+    }
+
+    @Test
+    void inserirAplicacao() {
+        
+        Aplicacao aplicacao = new Aplicacao("teste14", "2023-06-17", "cenario teste", 8f, 0);
+        aplicacao.setNome("teste2junit");
+       
+        boolean resultado = ComandosSistema.inserirAplicacao(aplicacao);
+
+        
+        assertTrue(resultado);
+    }
+
+   
+
+    @Test
+    void inserirDespesa() {
+        Despesa despesa = new Despesa("teste243", "2023-06-18", "cenario teste2", 4f);
+       
+        boolean resultado = ComandosSistema.inserirDespesa(despesa);
+        
+        assertTrue(resultado);
+    }
+
+   
+
+    @Test
+    void inserirEmprestimo() {
+        Emprestimo emprestimo = new Emprestimo("gabriel12", "2023-06-17", "cachorras2", 2f, 8f, 2, 2f);
+       
+        boolean resultado = ComandosSistema.inserirEmprestimo(emprestimo);
+        
+        assertTrue(resultado);
+    }
+
+
+    
+
+    @Test
+    void inserirReceita() {
+       
+        Receita receita = new Receita("teste52", "2023-06-20", "cenario teste42", 10f);
+      
+      
+       
+        boolean resultado = ComandosSistema.inserirReceita(receita);
+
+        
+        assertTrue(resultado);
+    }
+    
+    
+    @Test
+    public void testMostrarDiaria() {
+        String dataRef = "17/06/2023";
+        String resultadoEsperado = "Relatório diário";
+        
+        
+      
+        String resultado = ComandosSistema.gerarRelatorio("diario",dataRef);
+        
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testMostrarSemanal() {
+        String dataRef = "17/06/2023";
+        String resultadoEsperado = "Relatório semanal";
+        
+     
+        
+        
+      
+        String resultado = ComandosSistema.gerarRelatorio("semanal",dataRef);
+        
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testMostrarMensal() {
+        String dataRef = "06/2023";
+        String resultadoEsperado = "Relatório mensal"; 
+        
+        String resultado = ComandosSistema.gerarRelatorio("mensal",dataRef);
+        
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testMostrarAnual() {
+        String dataRef = "2023";
+        String resultadoEsperado = "Relatório anual"; 
+        
+        String resultado = ComandosSistema.gerarRelatorio("anual",dataRef);
+        
+        assertEquals(resultadoEsperado, resultado);
+    }
+    
+    
+}
